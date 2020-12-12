0341fad334a3ac704d2121c0cd4ba6bd2eda2528
ftg-gtf <67550388+ftg-gtf@users.noreply.github.com>
2020-12-12 22:33:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,43 +1,73 @@
-var bullet,wall;
-var speed,weight,thickness;
+
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1600,400);
-  speed = random(223,321);
-  weight = random(400,1500);
-  bullet = createSprite(50, 200, 50, 50);
-  wall = createSprite(1500,200,60,height/2);
-  wall.shapeColor = color(80,80,80)
-  bullet.velocityX = speed;
-  wall.debug = true;
+  createCanvas(1600, 400);
 
+  speed=random(223,321)
+  weight=random(30,52)
+
+
+  thickness=random(22,83)
+
+
+
+
+  
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
+
+ 
+  	
+    
+    wall=createSprite(1200, 200, thickness, height/2);  
+
+    wall.shapeColor=color(230,230,230);
+  //wall.shapeColor=color(80,80,80)
 }
+
 
 function draw() {
-  background(0,0,0);
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-  bullet.collide(wall);  
-   if(hasCollided(bullet,wall)){
-       bullet.velocityX = 0;
-       var damamge = 0.5 * weight * speed/(thickness * thickness * thickness);
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
-       if(damamge > 10){
-           wall.shapeColor = color(255,0,0);
-       }
+	
 
-       if(damamge < 10){
-        wall.shapeColor = color(0,255,0);
-    }
-   }
- 
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
+  }
+
+
   drawSprites();
-} 
-
-function hasCollided(bullet,wall){
-    bulletRightEdge - bullet.x + bullet.width;
-    wallLeftEdge = wall.x;  
-    if(bulletRightEdge >= wallLeftEdge){
-        return true;
-    }
-    return false;
+ 
 }
+
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
+}
+
+
